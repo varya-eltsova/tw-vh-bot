665b4457f1538497729eb98a3f0fd970a1675b8f
varya-eltsova <varyaeltsov@gmail.com>
2026-09-24 09:45:35 +0300

    format code

--- bot/tw_api.py
+++ bot/tw_api.py
@@ -13,21 +13,23 @@ def get_token(login, password, app_key):
     )
     return response.json()["token"]
 
+
 def _get(path, app_key, token):
-         response = requests.get(
-             f"{BASE_URL}{path}",
-             headers={"x-app-key": app_key, "Authorization": f"Bearer {token}"},
-             timeout=TIMEOUT,
-         )
-         return response.json()
+    response = requests.get(
+        f"{BASE_URL}{path}",
+        headers={"x-app-key": app_key, "Authorization": f"Bearer {token}"},
+        timeout=TIMEOUT,
+    )
+    return response.json()
+
 
 def get_balance(login, app_key, token):
-    return _get(f'/v1.1/finances/accounts/{login}', app_key, token)
+    return _get(f"/v1.1/finances/accounts/{login}", app_key, token)
 
 
 def get_sites(login, app_key, token):
-    return _get(f'/v1.1/sites/{login}', app_key, token)
-   
+    return _get(f"/v1.1/sites/{login}", app_key, token)
+
 
 def get_domains(login, app_key, token):
-    return _get(f'/v1/accounts/{login}/domains', app_key, token)
+    return _get(f"/v1/accounts/{login}/domains", app_key, token)
