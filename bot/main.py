7e26f133f6d40003ab5d4749292df71fed045033
varya-eltsova <varyaeltsov@gmail.com>
2026-09-24 12:22:18 +0300

    menu button clicks

--- bot/main.py
+++ bot/main.py
@@ -28,5 +28,30 @@ def start(message):
     )
 
 
+@tw_bot.callback_query_handler(func=lambda call: True)
+def on_menu_click(call):
+    tw_bot.answer_callback_query(call.id)
+
+    if call.data == "sites":
+        text = "Здесь будет список сайтов"
+    elif call.data == "domains":
+        text = "Здесь будет список доменов"
+    elif call.data == "balance":
+        text = "Здесь будет баланс"
+    else:
+        text = "Неизвестное сообщение"
+
+    try:
+        tw_bot.edit_message_text(
+            text,
+            call.message.chat.id,
+            call.message.message_id,
+            reply_markup=main_menu(),
+        )
+    except telebot.apihelper.ApiTelegramException as error:
+        if "message is not modified" not in error.description:
+            raise
+
+
 if __name__ == "__main__":
     tw_bot.infinity_polling()
