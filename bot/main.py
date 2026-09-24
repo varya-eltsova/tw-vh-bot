58fc49c530b99a08afe32d45336e5e67c1a0c7c4
varya-eltsova <varyaeltsov@gmail.com>
2026-09-24 15:26:53 +0300

    add auth steps

--- bot/main.py
+++ bot/main.py
@@ -2,6 +2,7 @@ import telebot
 from telebot import types
 
 from bot.config import TG_TOKEN
+from bot.db import get_auth_state, get_user, save_auth_state
 
 tw_bot = telebot.TeleBot(TG_TOKEN)
 
@@ -18,11 +19,48 @@ def main_menu():
 
 @tw_bot.message_handler(commands=["start"])
 def start(message):
-    tw_bot.send_message(
-        message.chat.id,
-        "Привет! Готов подсказать информацию по твоему аккаунту виртуального хостинга Timeweb.",
-        reply_markup=main_menu(),
-    )
+    telegram_id = message.from_user.id
+    user = get_user(telegram_id)
+
+    if user is not None:
+        tw_bot.send_message(
+            message.chat.id,
+            f"С возвращением! Аккаунт: {user.login}",
+            reply_markup=main_menu(),
+        )
+    else:
+        save_auth_state(telegram_id, "app_key")
+        tw_bot.send_message(
+            message.chat.id,
+            "Привет! Для начала авторизуйтесь.\nВведите ключ API:",
+        )
+
+
+@tw_bot.message_handler(content_types=["text"])
+def on_text(message):
+    telegram_id = message.from_user.id
+    state = get_auth_state(telegram_id)
+
+    if state is None:
+        tw_bot.send_message(message.chat.id, "Нажмите /start, чтобы начать.")
+        return
+
+    text = message.text.strip()
+
+    if state.step == "app_key":
+        tw_bot.delete_message(message.chat.id, message.message_id)
+        save_auth_state(telegram_id, "login", app_key=text)
+        tw_bot.send_message(message.chat.id, "Введите логин:")
+
+    elif state.step == "login":
+        save_auth_state(telegram_id, "password", login=text, app_key=state.app_key)
+        tw_bot.send_message(message.chat.id, "Введите пароль:")
+
+    elif state.step == "password":
+        tw_bot.delete_message(message.chat.id, message.message_id)
+        tw_bot.send_message(
+            message.chat.id, "Пароль получен. Авторизацию доделаем на следующем шаге."
+        )
 
 
 @tw_bot.callback_query_handler(func=lambda call: True)
