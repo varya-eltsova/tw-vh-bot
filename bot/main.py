0521e139b3a08103530400abe5387d5028d4e41a
varya-eltsova <varyaeltsov@gmail.com>
2026-09-24 11:10:30 +0300

    add inline main menu

--- bot/main.py
+++ bot/main.py
@@ -2,10 +2,21 @@ import os
 
 import telebot
 from dotenv import load_dotenv
+from telebot import types
 
 load_dotenv()
 
 tw_bot = telebot.TeleBot(os.getenv("TG_TOKEN"))
+
+
+def main_menu():
+    markup = types.InlineKeyboardMarkup()
+    markup.row(
+        types.InlineKeyboardButton("Мои сайты", callback_data="sites"),
+        types.InlineKeyboardButton("Мой баланс", callback_data="balance"),
+        types.InlineKeyboardButton("Мои домены", callback_data="domains"),
+    )
+    return markup
 
 
 @tw_bot.message_handler(commands=["start"])
@@ -13,7 +24,9 @@ def start(message):
     tw_bot.send_message(
         message.chat.id,
         "Привет! Готов подсказать информацию по твоему аккаунту виртуального хостинга Timeweb.",
+        reply_markup=main_menu(),
     )
+
 
 if __name__ == "__main__":
     tw_bot.infinity_polling()
